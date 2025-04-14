62f9ecd617fd1e748046f17567807286f8e58c3b
itagaki <itagaki_shun@vitallead.co.jp>
2025-04-15 08:47:01 +0900

    プログラムを修正

--- 01.fizzbuzz/ex_fizzbuzz.js
+++ 01.fizzbuzz/ex_fizzbuzz.js
@@ -1,16 +1,13 @@
 let cnt = 0;
-while(cnt < 20){
-    cnt++;
-    if(cnt % 3 == 0 && cnt % 5 == 0){
-        console.log("FizzBuzz");
-    }
-    else if(cnt % 3 == 0){
-        console.log("Fizz");
-    }
-    else if(cnt % 5 == 0){
-        console.log("Buzz");
-    }
-    else{
+while (cnt < 20) {
+  cnt++;
+  if (cnt % 3 == 0 && cnt % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (cnt % 3 == 0) {
+    console.log("Fizz");
+  } else if (cnt % 5 == 0) {
+    console.log("Buzz");
+  } else {
     console.log(cnt);
-    };
+  }
 }
