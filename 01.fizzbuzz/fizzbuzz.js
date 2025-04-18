ccc601846f79247056eb19ff5d354bfbc17a5677
Gaki27 <itagaki_shun@vitallead.co.jp>
2025-04-18 13:41:16 +0900

    比較演算子の修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -3,11 +3,11 @@
 let cnt = 0;
 while (cnt < 20) {
   cnt++;
-  if (cnt % 3 == 0 && cnt % 5 == 0) {
+  if (cnt % 3 === 0 && cnt % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (cnt % 3 == 0) {
+  } else if (cnt % 3 === 0) {
     console.log("Fizz");
-  } else if (cnt % 5 == 0) {
+  } else if (cnt % 5 === 0) {
     console.log("Buzz");
   } else {
     console.log(cnt);
