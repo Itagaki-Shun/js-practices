c2f5fc5bc0317efa35c7f0d7980215669e4ffcd0
itagaki <itagaki_shun@vitallead.co.jp>
2025-04-18 10:50:17 +0900

    入力されたコマンドライン引数から年月を求める処理の追加、確認

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -8,10 +8,27 @@ let y = 0;
 //コマンドライン引数を格納する配列を用意
 for (let i = 0; i < process.argv.length; ++i) {
   //引数を確認できる処理
-  console.log(i + ': ' + process.argv[i]);
+  //console.log(i + ': ' + process.argv[i]);
   if (process.argv[i] == '-y') {
     y = process.argv[i + 1];
   } else if (process.argv[i] == '-m') {
     m = process.argv[i + 1];
   }
 }
+
+//年月日、曜日を求める
+let today = new Date(y, m);
+//何も指定しない場合は今月を求める
+if (y == 0 && y == 0) {
+  today = new Date();
+}
+let year = today.getFullYear();
+let month = today.getMonth() + 1;
+let day = today.getDate();
+let day_of_week = today.getDay();
+
+console.log(today);
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(day_of_week);
