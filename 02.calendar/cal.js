310c675f3499313e2c4a158dfa963e6b26020bd2
itagaki <itagaki_shun@vitallead.co.jp>
2025-04-30 09:16:01 +0900

    変数名の修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,46 +1,44 @@
 #! /usr/bin/env node
 
-let Argument_Year;
-let Argument_Month = -1;
+let argumentYear;
+let argumentMonth = -1;
 
 for (let i = 0; i < process.argv.length; ++i) {
   if (process.argv[i] === `-y`) {
-    Argument_Year = process.argv[i + 1];
+    argumentYear = process.argv[i + 1];
   }
 }
 for (let i = 0; i < process.argv.length; ++i) {
   if (process.argv[i] === `-m`) {
-    Argument_Month += Number(process.argv[i + 1]);
+    argumentMonth += Number(process.argv[i + 1]);
   }
 }
 
-let today = new Date();
-
-//今年と今月を求め、それぞれの変数に格納
+let day = new Date();
 let nowYaer = new Date().getFullYear();
 let nowMonth = new Date().getMonth();
 
-if (Argument_Year > 0 && Argument_Month > 0) {
-  today = new Date(Argument_Year, Argument_Month);
-} else if (Argument_Year > 0) {
-  today = new Date(Argument_Year, nowMonth);
-} else if (Argument_Month > 0) {
-  today = new Date(nowYaer, Argument_Month);
+if (argumentYear > 0 && argumentMonth > 0) {
+  day = new Date(argumentYear, argumentMonth);
+} else if (argumentYear > 0) {
+  day = new Date(argumentYear, nowMonth);
+} else if (argumentMonth > 0) {
+  day = new Date(nowYaer, argumentMonth);
 }
 
-let year = today.getFullYear();
-let month = today.getMonth() + 1;
+const year = day.getFullYear();
+const month = day.getMonth() + 1;
 
 console.log(`      ${month}月 ${year}`);
 
-let firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+let firstDayOfMonth = new Date(day.getFullYear(), day.getMonth(), 1);
 let f_day_of_week = firstDayOfMonth.getDay();
-let lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+let lastDayOfMonth = new Date(day.getFullYear(), day.getMonth() + 1, 0);
 let l_day = lastDayOfMonth.getDate();
 
-const days = [`日`, `月`, `火`, `水`, `木`, `金`, `土`];
+const days = [`日`, ` 月`, ` 火`, ` 水`, ` 木`, ` 金`, ` 土`];
 for (let count = 0; count < 7; count++) {
-  process.stdout.write(`${days[count]} `);
+  process.stdout.write(`${days[count]}`);
 }
 
 console.log(``);
@@ -49,22 +47,22 @@ if (f_day_of_week > 0) {
 }
 
 let space = `   `;
-for (let day = 1; day <= l_day; day++) {
-  if (f_day_of_week > 0 && day === 1) {
+for (let i = 1; i <= l_day; i++) {
+  if (f_day_of_week > 0 && i === 1) {
     for (let i = 1; i < f_day_of_week; i++) {
       process.stdout.write(space);
     }
   }
   if (
-    (day < 10 && f_day_of_week === 0) ||
-    (day > 10 && f_day_of_week !== 0) ||
-    (day === 10 && f_day_of_week !== 0)
+    (i < 10 && f_day_of_week === 0) ||
+    (i > 10 && f_day_of_week !== 0) ||
+    (i === 10 && f_day_of_week !== 0)
   ) {
-    process.stdout.write(` ${day}`);
+    process.stdout.write(` ${i}`);
   } else if (f_day_of_week === 0) {
-    process.stdout.write(`${day}`);
+    process.stdout.write(`${i}`);
   } else {
-    process.stdout.write(`  ${day}`);
+    process.stdout.write(`  ${i}`);
   }
 
   f_day_of_week++;
